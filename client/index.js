6efe9e459596ab391e34cbb18d4bee74e113c813
amboob223 <monk8896@gmail.com>
2023-06-20 23:21:27 -0400

    yhuj

--- client/index.js
+++ client/index.js
@@ -16,7 +16,7 @@ button.addEventListener("click", async (event) => {
         }  // because ima make form data not json remove the line below we need to switch to json instead of multipart form data
 
 
-        const response = await fetch("/", {
+        const response = await fetch("https://portfolio-o78g.onrender.com", {
             method: "POST",
             headers: { "Content-type": "Application/json" },
             body: JSON.stringify(body)
