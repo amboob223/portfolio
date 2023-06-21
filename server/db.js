ecec04129a99f14bc75a8d35cc60b060f4c8d558
amboob223 <monk8896@gmail.com>
2023-06-21 01:23:28 -0400

    yhuj

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const POOL = require("pg").Pool
 const pool = new POOL({
-    user: "postgres",
-    password: "8896",
+    user: "port_rf7h_user",
+    password: "jq5CXXQiLSOmPWqgwMnqhaCYwizz6N4p",
     port: 5432,
-    database: "port",
-    host: "localhost"
+    database: "port_rf7h",
+
 });
 
 module.exports = pool
