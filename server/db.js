d85e19bcab694679d3a978dc0ba5124e3b0225e1
amboob223 <monk8896@gmail.com>
2023-06-23 02:15:33 -0400

    yhuj

--- server/db.js
+++ server/db.js
@@ -1,10 +1,9 @@
 const POOL = require("pg").Pool
+require("dotenv").config()
+
 const pool = new POOL({
-    user: "port_rf7h_user",
-    password: "jq5CXXQiLSOmPWqgwMnqhaCYwizz6N4p",
-    port: 5432,
-    database: "port_rf7h",
-    host: "dpg-ci98a918g3ne2egvnabg-a"
+
+    connectionString: process.env.DATABASE_URL
 
 });
 
