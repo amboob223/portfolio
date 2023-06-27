d9b69cc763cdb2dc30ffdd1554627a44e4df908d
amboob223 <monk8896@gmail.com>
2023-06-27 16:31:08 -0400

    yhuj

--- server/db.js
+++ server/db.js
@@ -3,9 +3,7 @@ require("dotenv").config()
 
 const pool = new POOL({
     host: "dpg-ci98a918g3ne2egvnabg-a.ohio-postgres.render.com",
-    ssl: {
-        rejectUnauthorized: false
-    },
+    ssl: true,
     database: "port_rf7h",
     password: "jq5CXXQiLSOmPWqgwMnqhaCYwizz6N4p",
     user: "port_rf7h_user",
