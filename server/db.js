078467e74b7023b312a68f04634fca8c49ddb11e
amboob223 <monk8896@gmail.com>
2023-06-26 20:28:11 -0400

    yhuj

--- server/db.js
+++ server/db.js
@@ -1,10 +1,14 @@
 const POOL = require("pg").Pool;
 require("dotenv").config()
 
-const pool = new POOL({
-
-    connectionString: process.env.DATABASE_URL
-
-});
+const pool = new POOL(
+    {
+        host: "dpg-ci98a918g3ne2egvnabg-a.ohio-postgres.render.com",
+        database: "port_rf7h",
+        port: 5432,
+        user: "port_rf7h_user",
+        password: "jq5CXXQiLSOmPWqgwMnqhaCYwizz6N4p"
+    }
+);
 
 module.exports = pool
