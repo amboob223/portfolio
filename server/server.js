efa31920c9e6d5d278c1fbebd5d1be6034a5bcb4
amboob223 <monk8896@gmail.com>
2023-06-28 19:25:59 -0400

    gliz

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 });
 
 // this is the work route
-app.post("/", async (req, res) => {
+app.post("/work", async (req, res) => {
     try {
         const { name, phone, email, message } = req.body;
         const newData = await pool.query(
