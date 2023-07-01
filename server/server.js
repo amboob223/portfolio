cdb975cc948879f81e956d0b14305f246919cb6a
amboob223 <monk8896@gmail.com>
2023-07-01 19:01:41 -0400

    ddd

--- server/server.js
+++ server/server.js
@@ -1,44 +1,89 @@
 
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { createLogger, transports } = require("winston");
 const pool = require("./db");
 
-//middleware
+// Middleware
 app.use(cors());
-app.use(express.json())//json parse 
+app.use(express.json());
 
-// post
-// this is the root  route
+// Create a logger instance
+const logger = createLogger({
+    transports: [
+        new transports.Console(), // Output logs to the console
+        new transports.File({ filename: "error.log", level: "error" }) // Output error logs to a file
+    ]
+});
+
+// Root route
 app.get("/", (req, res) => {
     res.send("Welcome to the portfeeelio website!");
 });
 
-// this is the work route
+// Work route
 app.post("/work", async (req, res) => {
     try {
         const { name, phone, email, message } = req.body;
         const newData = await pool.query(
-            "INSERT INTO work (name,phone,email,message) VALUES($1,$2,$3,$4) RETURNING *",
+            "INSERT INTO work (name, phone, email, message) VALUES ($1, $2, $3, $4) RETURNING *",
             [name, phone, email, message]
         );
-        res.json(newData.rows)
-        fetchData()
 
-        console.log("New data inserted:", newData.rows);
+        // Log the new data
+        logger.info("New data inserted:", newData.rows);
+
+        res.json(newData.rows);
     } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: "internal server error" })
-    } // you got to do status 500 .json and give it an object with the error property and say internal server errror
+        logger.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
-async function fetchData() {
-    try {
-        const result = await pool.query("SELECT * FROM work");
-        console.log(result.rows);
-    } catch (err) {
-        console.error(err);
-    }
-}
-// this port is for the server
+// Export the Express app
+module.exports = app;
+
+
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const pool = require("./db");
+
+// //middleware
+// app.use(cors());
+// app.use(express.json())//json parse 
+
+// // post
+// // this is the root  route
+// app.get("/", (req, res) => {
+//     res.send("Welcome to the portfeeelio website!");
+// });
+
+// // this is the work route
+// app.post("/work", async (req, res) => {
+//     try {
+//         const { name, phone, email, message } = req.body;
+//         const newData = await pool.query(
+//             "INSERT INTO work (name,phone,email,message) VALUES($1,$2,$3,$4) RETURNING *",
+//             [name, phone, email, message]
+//         );
+//         res.json(newData.rows)
+//         fetchData()
+
+//         console.log("New data inserted:", newData.rows);
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).json({ error: "internal server error" })
+//     } // you got to do status 500 .json and give it an object with the error property and say internal server errror
+// });
+
+// async function fetchData() {
+//     try {
+//         const result = await pool.query("SELECT * FROM work");
+//         console.log(result.rows);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
+// // this port is for the server
