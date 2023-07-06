ed450b92745c7b9b8de76ffbb07723b38cc1a09d
amboob223 <monk8896@gmail.com>
2023-07-06 15:00:51 -0400

    ddd

--- server/server.js
+++ server/server.js
@@ -40,9 +40,9 @@ app.post("/work", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server listening on port ${PORT}`);
+// });
 
 module.exports = app;
