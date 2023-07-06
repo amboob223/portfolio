dee7b5cdd8bd45b04f39dc3d8361981bbe0294fb
amboob223 <monk8896@gmail.com>
2023-07-06 14:56:31 -0400

    ddd

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
-
+const port = process.env.PORT || 3001;
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
