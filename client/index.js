e7b1fc1618d5a5b6da489d109fc41ba3af70a64d
amboob223 <monk8896@gmail.com>
2023-07-08 18:46:20 -0400

    uuu

--- client/index.js
+++ client/index.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
             }  // because ima make form data not json remove the line below we need to switch to json instead of multipart form data
 
 
-            const response = await fetch("https://portfolio-o78g.onrender.com/work", {
+            const response = await fetch("https://portfolio-o78g.onrender.com", {
                 method: "POST",
                 headers: { "Content-type": "Application/json" },
                 body: JSON.stringify(body)
