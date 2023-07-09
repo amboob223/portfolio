163b6d8fb4292dd317a4e34e432fa494cfdff7b0
amboob223 <monk8896@gmail.com>
2023-07-09 13:21:03 -0400

    sad

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.post("/work", async (req, res) => {
 // Start the server
 // const PORT = process.env.PORT || 3000;
 
-app.listen(3001, () => {
+app.listen(5000, () => {
     console.log(`Server listening on port`);
 });
 // server.keepAliveTimeout = 120 * 1000;
