e00a1d057f69a39aea2e042e5aa38bbf07bc341c
amboob223 <monk8896@gmail.com>
2023-07-09 13:11:39 -0400

    sad

--- client/index.js
+++ client/index.js
@@ -19,8 +19,9 @@ document.addEventListener("DOMContentLoaded", () => {
             }  // because ima make form data not json remove the line below we need to switch to json instead of multipart form data
 
 
-            const response = await fetch("https://localhost:5000/work", {
+            const response = await fetch("https://localhost:3000/work", {
                 method: "POST",
+
                 body: JSON.stringify(body)
             })
             console.log("work")
--- server/server.js
+++ server/server.js
@@ -36,9 +36,9 @@ app.post("/work", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(5000, () => {
+app.listen(3000, () => {
     console.log(`Server listening on port ${PORT}`);
 });
 // server.keepAliveTimeout = 120 * 1000;
